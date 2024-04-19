497e085a5335e86286da2e2062e4cde5cb064fbb
Paul Irwin <irwinj@southern.edu>
2024-04-19 14:54:25 -0400

    Added base test class for generating scripts and source.

---
+++ SQuiL.Tests/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+
+using System.Runtime.CompilerServices;
+
+using static Microsoft.CodeAnalysis.SourceGeneratorHelper;
+
+namespace SQuiL.Tests;
+
+public abstract class BaseTest
+{
+	protected static string TestHeader(
+		IEnumerable<string> attributes = default!,
+		Func<string, string> callback = default!,
+		[CallerMemberName] string name = default!)
+	{
+		attributes ??= [name];
+		callback ??= p => $$"""
+			[{{QueryAttributeName}}(QueryFiles.{{p}})]
+			""";
+
+		return $$"""
+			using Microsoft.Extensions.Configuration;
+			using {{NamespaceName}};
+		
+			namespace TestCase;
+		
+			{{string.Join("", attributes.Select(callback))}}
+			public partial class {{name}}DataContext(IConfiguration Configuration) : {{BaseDataContextClassName}}(Configuration)
+			{
+			}
+			""";
+	}
+}
