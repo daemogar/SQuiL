50f4ce1caf4c207fdb7e5c2f876eb4f89e5e2637
Paul Irwin <irwinj@southern.edu>
2024-04-19 14:54:32 -0400

    Added test for when a float value is created.

---
+++ SQuiL.Tests/TableWithFloatPropertyTests/TableWithFloatPropertyTests.cs
@@ -0,0 +1,17 @@
+﻿namespace SQuiL.Tests;
+
+public class TableWithFloatPropertyTests : BaseTest
+{
+	[Fact]
+	public Task DoubleNumberTest()
+	{
+		//Debugger.Launch();
+
+		var name = nameof(DoubleNumberTest);
+		return TestHelper.Verify([TestHeader([name])], [$$"""
+			--Name: {{name}}
+			Declare @Returns_Answers table(Response float);
+			Use [Database];
+			"""]);
+	}
+}
