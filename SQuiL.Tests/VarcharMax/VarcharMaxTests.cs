520a07970e3b4f1f4e192f0753ab9e017044c8fe
Paul Irwin <irwinj@southern.edu>
2026-04-07 14:09:01 -0400

    Added VarcharMax test case.

---
+++ SQuiL.Tests/VarcharMax/VarcharMaxTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace SQuiL.Tests.VarcharMax;
+
+public class VarcharMaxTests : BaseTest
+{
+	[Fact]
+	public Task InputParameterTakesMaxStringConvertToTableArray()
+	{
+		Debugger.Launch();
+
+		//--Declare	@Params_LongText table ([Error] varchar(max));
+
+		var name = nameof(InputParameterTakesMaxStringConvertToTableArray);
+		return TestHelper.Verify([TestHeader([name])], [$$"""
+			--Name: {{name}}
+			
+			Declare	@Param_LongText varchar(max);
+		
+			Use [Database];
+			
+			Select 1;
+			"""]);
+	}
+}
